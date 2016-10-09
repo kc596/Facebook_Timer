a9a5e743149fb790e932b21e6f0a050c7fc5ad96
kc596 <chaudhary.kc.kunal@gmail.com>
2016-10-09 13:20:13 +0530

    Removed dependency on facebook class for injecting timer

--- timer.js
+++ timer.js
@@ -1,21 +1,28 @@
 /**** Timer variables ****/
 var h1, h2, m1, m2, s1, s2;
 
-/**** Function to get old timer value ****/
+/**** Retriving timer's old value ****/
 chrome.storage.local.get(['_h1','_h2','_m1','_m2','_s1','_s2'], function(result){
-	h1=result['_h1'];
-	h2=result['_h2'];
-	m1=result['_m1'];
-	m2=result['_m2'];
-	s1=result['_s1'];
-	s2=result['_s2'];
+	if(result['_h1']==undefined){
+		h1=0; h2=0; m1=0; m2=0; s1=0; s2=0;
+	}
+	else{
+		h1=result['_h1'];
+		h2=result['_h2'];
+		m1=result['_m1'];
+		m2=result['_m2'];
+		s1=result['_s1'];
+		s2=result['_s2'];
+	}
 });
 var time = h1+""+h2+" : "+m1+""+m2+" : "+s1+""+s2;
 
 /**** Chossing up the place to display timer ****/
-var myElement = document.getElementById("blueBarDOMInspector");
+var myElement = document.createElement("div");
+myElement.setAttribute("class", "timer-style");
+document.getElementsByTagName("body")[0].appendChild(myElement);
 var child = document.createElement("div");
-child.setAttribute("class","timer-style");
+child.setAttribute("class","timer_bg");
 child.innerHTML=time;
 
 /**** Function to display timer and store its value ****/
